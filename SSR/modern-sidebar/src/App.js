8252103bbe226273f633d1e2517b08889726503e
John Oosthuizen <65812622+johnoos@users.noreply.github.com>
2020-09-12 12:07:53 +0200

    added modern-sidebar-master

---
+++ SSR/modern-sidebar/src/App.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+const items = [
+  { name: 'home', label: 'Home' },
+  {
+    name: 'billing',
+    label: 'Billing',
+    items: [
+      { name: 'statements', label: 'Statements' },
+      { name: 'reports', label: 'Reports' },
+    ],
+  },
+  {
+    name: 'settings',
+    label: 'Settings',
+    items: [
+      { name: 'profile', label: 'Profile' },
+      { name: 'insurance', label: 'Insurance' },
+      {
+        name: 'notifications',
+        label: 'Notifications',
+        items: [
+          { name: 'email', label: 'Email' },
+          {
+            name: 'desktop',
+            label: 'Desktop',
+            items: [
+              { name: 'schedule', label: 'Schedule' },
+              { name: 'frequency', label: 'Frequency' },
+            ],
+          },
+          { name: 'sms', label: 'SMS' },
+        ],
+      },
+    ],
+  },
+]
+
+function App() {
+  return (
+    <div>
+      <Sidebar items={items} />
+    </div>
+  )
+}
+
+export default App
